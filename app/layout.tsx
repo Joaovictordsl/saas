883981ef44bd60d2228f3d921ec958165d34cf2a
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-03-07 01:32:36 -0300

    dashboard & root

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        Coming from the main layout!
+        NavBar
         {children}
+        Footer
       </body>
     </html>
   );
