99c4bd79f326db313ae23547cc5d28f5221bae0a
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-03-07 19:09:44 -0300

    fix: add missing routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,10 +4,21 @@ const Home = () => {
 
     return (
         <main>
-            <div>Welcome to nextJS!</div>
+            <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-x1">Welcome to nextJS!</div>
             <Hello/>
         </main>
     );
+}
+
+async function Page(){
+    const response = await fetch("http://localhost:3000/books");
+    const books = await response.json();
+
+    return (
+        <main>
+            <code>{JSON.stringify(books, null, 2)}</code>
+        </main>
+    )
 }
 
 export default Home
---
+++ app/api/db.ts
@@ -0,0 +1,6 @@
+const books = [
+    {id: 1, name: "Atomic Habits"},
+    {id: 2, name: "Deep Work"}
+]
+
+export default books;
